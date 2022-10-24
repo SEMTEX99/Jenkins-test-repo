ae653ef387653a972c8d68c38af8b6adeb3b2620
SEMTEX99 <sinisamilosevic19@gmail.com>
2022-10-24 14:48:46 +0200

    zglajz mentalni sa tuborgom na mozgu

--- src/main/java/com/semtex/DevopsIntegrationApplication.java
+++ src/main/java/com/semtex/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome to jenkins totalno ludilo, dobicete tuborg na mozgu";
 	}
 
 	public static void main(String[] args) {
