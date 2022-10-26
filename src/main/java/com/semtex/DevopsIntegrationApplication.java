662759834459cdc9cfc0d16c47d7c861d5ce9285
SEMTEX99 <sinisamilosevic19@gmail.com>
2022-10-26 10:33:02 +0200

    added remote exec jenkinsfile

--- src/main/java/com/semtex/DevopsIntegrationApplication.java
+++ src/main/java/com/semtex/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to jenkins totalno ludilo, dobicete tuborg na mozgu";
+		return "welcome to jenkins";
 	}
 
 	public static void main(String[] args) {
